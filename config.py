dc0047c51fa165d5f3015df76bc55da9c88a64a1
xiaowang012 <1300202481@qq.com>
2022-02-16 14:20:06 +0800

    将邮箱用户密码放置在代码外部

--- config.py
+++ config.py
@@ -1,10 +1,24 @@
 #coding=utf-8
 import os
 import sys
-import platform
-
+import json
 basedir = os.path.abspath(os.path.dirname(__file__))
 system_type = sys.platform
+
+#get Parameters
+paras = None
+para_path = basedir +os.sep + "parameters.json"
+if os.path.isfile(para_path) == True:
+    try:
+        with open(para_path) as f:
+            paras = json.load(f)
+            
+    except:
+        Error2 = Exception("读取json异常!")
+        raise Error2
+else:
+    Error1 = Exception("读取参数失败!")
+    raise Error1
 
 class DataBaseConfig(object):
     DEBUG = True
@@ -15,11 +29,12 @@ class DataBaseConfig(object):
         #linux
         SQLALCHEMY_DATABASE_URI =   "sqlite:///"+os.path.join(basedir +"/database/", "database123.db")
 
+
 class EmailConfig(object):
     MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = '1300202481@qq.com'
-    MAIL_PASSWORD = 'univlugrxnlbhcdd'
+    MAIL_USERNAME = paras["MAIL_USERNAME"]
+    MAIL_PASSWORD = paras["MAIL_PASSWORD"]
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
